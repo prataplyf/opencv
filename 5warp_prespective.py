92a7bc3dd4bd35bba4f928e3c804a2f1e027c3fa
prataplyf <prataplyf@gmail.com>
2020-09-03 20:35:00 +0530

    chapter 5

--- 5warp_prespective.py
+++ 5warp_prespective.py
@@ -6,6 +6,7 @@ width, height = 150, 250
 # [[467, 290], [585, 352], [370, 443], [490, 515]]
 # [[270, 305], [382, 367], [171, 465], [285,532]]
 """ a1, a2, a3, a4 == [x-axis, y-axis]
+    top-left, top-right, bottom-right, and bottom-left
     [[a1], [a2], [a3], [a4]]
     a1 ------- a2
     ------------
@@ -13,6 +14,12 @@ width, height = 150, 250
     ------------
     a3 -------- a4
 """
+# now that we have our screen contour, we need to determine
+# the top-left, top-right, bottom-right, and bottom-left
+# points so that we can later warp the image -- we'll start
+# by reshaping our contour to be our finals and initializing
+# our output rectangle in top-left, top-right, bottom-right,
+# and bottom-left order
 
 pts1 = np.float32([[467, 290], [585, 352], [370, 443], [490, 515]])
 pts2 = np.float32([[0,0], [width, 0], [0, height], [width,height]])
